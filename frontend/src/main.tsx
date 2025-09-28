8642a0e227f8fb364edfd0fe6d26e9162243bd01
xXrederXx <morgenthalerthierry@gmail.com>
2025-09-28 20:55:06 +0200

    Add routing and empty pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './root.css'
 import './main.css'
 import App from './App.tsx'
+import Signup from './user/signup.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontend/src/user/signup.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../root.css'
+
+export default function Signup() {
+  return (
+    <div>signup</div>
+  )
+}
